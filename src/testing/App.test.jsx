e53f0f285b7e05dcce8dc573256fab1689cce113
tococo852 <tococo852@gmail.com>
2025-12-03 13:45:28 -0300

    fixed test setup not working

--- src/testing/App.test.jsx
+++ src/testing/App.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import App from '../app/App.jsx';
+import App from '../App/App.jsx';
 describe('something truthy and falsy', () => {
   it('true to be true', () => {
     expect(true).toBe(true);
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: '../testing/setup.js',
+      setupFiles: './src/testing/setup.js',
   },
 })
