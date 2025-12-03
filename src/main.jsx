6cfc896f54dfb555584cb5fa75da12530b14ab61
Tococo852 <tococo852@gmail.com>
2025-12-03 15:16:12 -0300

    added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
-import routes from './app/routes.jsx'
+import routes from './App/routes.jsx'
 import './index.css'
-import App from './App/App.jsx'
 
 const router= createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
