25d0c36f7878092a2013d9fd98192506dfcd8995
tococo852 <tococo852@gmail.com>
2025-12-03 14:00:11 -0300

    working on router feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import routes from './app/routes.jsx'
 import './index.css'
 import App from './App/App.jsx'
+
+const router= createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
